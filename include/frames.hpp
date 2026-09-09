a1b1dbf7f899bfc678322fe64636bc73c2623670
Les-S-niK <lessnik343@gmail.com>
2026-09-09 23:04:37 +0300

    feat(core): Implement set/get methods for EthernetFrame and ArpFrame.
    - Add new methods to fill ethernet and arp frames.
    - Add tests for all the new methods.

--- include/frames.hpp
+++ include/frames.hpp
@@ -8,6 +8,25 @@
 
 #include "number_usings.hpp"
 
+template <typename Iter>
+constexpr auto u16ToU8Array(Iter begin, u16 number) noexcept -> void {
+    if (begin == nullptr or std::next(begin, 1) == nullptr) [[unlikely]] {
+        std::terminate();
+    }
+    constexpr u8 byte_size = 0x08;
+    constexpr u8 max_byte = 0xFF;
+    *begin = number >> byte_size;
+    *(std::next(begin, 1)) = number & max_byte;
+}
+
+[[nodiscard]] constexpr auto u16ToU8Array(u16 number) noexcept
+    -> std::array<u8, 2> {
+    std::array<u8, 2> result{};
+    u16ToU8Array(result.begin(), number);
+
+    return result;
+}
+
 template <typename Frame, u16 FrameSize, u16 HeaderSize, u16 PayloadSize>
 concept frame_trait = requires(Frame frame, std::array<u8, HeaderSize> header,
                                std::array<u8, PayloadSize> payload) {
@@ -20,32 +39,27 @@ concept frame_trait = requires(Frame frame, std::array<u8, HeaderSize> header,
 
 template <u16 HeaderSize, u16 PayloadSize>
 class NetworkFrame {
+    static constexpr u8 hwaddr_size = 6;
+    static constexpr u8 praddr_size = 4;
+
+   public:
     using header_t = std::array<u8, HeaderSize>;
     using payload_t = std::array<u8, PayloadSize>;
     using frame_t = std::array<u8, HeaderSize + PayloadSize>;
+    using hwaddr_t = std::array<u8, hwaddr_size>;
+    using praddr_t = std::array<u8, praddr_size>;
 
-   public:
-    auto setHeader(header_t header) noexcept -> NetworkFrame& {
-        _header = header;
-        return *this;
-    }
-    auto setPayload(payload_t payload) noexcept -> NetworkFrame& {
-        _payload = payload;
-        return *this;
-    }
-    [[nodiscard]] auto getHeader() const noexcept -> header_t {
-        return _header;
-    }
-    [[nodiscard]] auto getPayload() const noexcept -> payload_t {
-        return _payload;
-    }
-    [[nodiscard]] auto toU8Array() const noexcept -> frame_t {
-        frame_t result{};
-        std::copy(_header.cbegin(), _header.cend(), result.begin());
-        std::copy(_payload.cbegin(), _payload.cend(),
-                  std::next(result.begin(), HeaderSize));
-        return result;
-    }
+    constexpr auto setHeader(header_t header) noexcept -> NetworkFrame&;
+    constexpr auto setPayload(payload_t payload) noexcept -> NetworkFrame&;
+    [[nodiscard]] constexpr auto getHwAddrSize() const noexcept -> u16;
+    [[nodiscard]] constexpr auto getPrAddrSize() const noexcept -> u16;
+    [[nodiscard]] constexpr auto getHeader() const noexcept -> header_t;
+    [[nodiscard]] constexpr auto getPayload() const noexcept -> payload_t;
+    [[nodiscard]] constexpr auto toU8Array() const noexcept -> frame_t;
+
+   protected:
+    [[nodiscard]] constexpr auto getHeaderRef() noexcept -> header_t&;
+    [[nodiscard]] constexpr auto getPayloadRef() noexcept -> payload_t&;
 
    private:
     std::array<u8, HeaderSize> _header{};
@@ -55,8 +69,18 @@ class NetworkFrame {
 constexpr u8 ethernet_header_size = 14;
 template <u16 PayloadSize>
 class EthernetFrame : public NetworkFrame<ethernet_header_size, PayloadSize> {
+    static constexpr u8 source_hwaddr_offset = 6;
+    static constexpr u8 payload_type_offset = 12;
+
    public:
     static constexpr u8 header_size = ethernet_header_size;
+
+    constexpr auto setHeaderDestinationHwAddr(
+        EthernetFrame::hwaddr_t hwaddr) noexcept -> EthernetFrame&;
+    constexpr auto setHeaderSourceHwAddr(
+        EthernetFrame::hwaddr_t hwaddr) noexcept -> EthernetFrame&;
+    constexpr auto setHeaderPayloadType(u16 payload_type) noexcept
+        -> EthernetFrame&;
 };
 
 constexpr u8 arp_header_size = 8;
@@ -65,7 +89,164 @@ class ArpFrame : public NetworkFrame<arp_header_size, arp_payload_size> {
    public:
     static constexpr u8 header_size = arp_header_size;
     static constexpr u8 payload_size = arp_payload_size;
+
+    static constexpr u8 header_hw_type_offset = 0;
+    static constexpr u8 header_pr_type_offset = 2;
+    static constexpr u8 header_hw_size_offset = 4;
+    static constexpr u8 header_pr_size_offset = 5;
+    static constexpr u8 header_opcode_offset = 6;
+
+    static constexpr u8 payload_src_hwaddr_offset = 0;
+    static constexpr u8 payload_src_praddr_offset = 6;
+    static constexpr u8 payload_dst_hwaddr_offset = 10;
+    static constexpr u8 payload_dst_praddr_offset = 16;
+
+    constexpr auto setHeaderHwAddrType(u16 hwaddr_type) noexcept -> ArpFrame&;
+    constexpr auto setHeaderPrAddrType(u16 praddr_type) noexcept -> ArpFrame&;
+    constexpr auto setHeaderHwAddrSize(u8 hwaddr_size) noexcept -> ArpFrame&;
+    constexpr auto setHeaderPrAddrSize(u8 praddr_size) noexcept -> ArpFrame&;
+    constexpr auto setHeaderOpcode(u16 opcode) noexcept -> ArpFrame&;
+
+    constexpr auto setPayloadSourceHwAddr(
+        ArpFrame::hwaddr_t src_hwaddr) noexcept -> ArpFrame&;
+    constexpr auto setPayloadSourcePrAddr(
+        ArpFrame::praddr_t src_praddr) noexcept -> ArpFrame&;
+    constexpr auto setPayloadDestinationHwAddr(
+        ArpFrame::hwaddr_t dst_hwaddr) noexcept -> ArpFrame&;
+    constexpr auto setPayloadDestinationPrAddr(
+        ArpFrame::praddr_t dst_praddr) noexcept -> ArpFrame&;
 };
+
+template <u16 HeaderSize, u16 PayloadSize>
+constexpr auto NetworkFrame<HeaderSize, PayloadSize>::setHeader(
+    header_t header) noexcept -> NetworkFrame& {
+    _header = header;
+    return *this;
+}
+template <u16 HeaderSize, u16 PayloadSize>
+constexpr auto NetworkFrame<HeaderSize, PayloadSize>::setPayload(
+    payload_t payload) noexcept -> NetworkFrame& {
+    _payload = payload;
+    return *this;
+}
+template <u16 HeaderSize, u16 PayloadSize>
+[[nodiscard]] constexpr auto
+NetworkFrame<HeaderSize, PayloadSize>::getHwAddrSize() const noexcept -> u16 {
+    return hwaddr_size;
+}
+template <u16 HeaderSize, u16 PayloadSize>
+[[nodiscard]] constexpr auto
+NetworkFrame<HeaderSize, PayloadSize>::getPrAddrSize() const noexcept -> u16 {
+    return praddr_size;
+}
+template <u16 HeaderSize, u16 PayloadSize>
+[[nodiscard]] constexpr auto NetworkFrame<HeaderSize, PayloadSize>::getHeader()
+    const noexcept -> header_t {
+    return _header;
+}
+template <u16 HeaderSize, u16 PayloadSize>
+[[nodiscard]] constexpr auto NetworkFrame<HeaderSize, PayloadSize>::getPayload()
+    const noexcept -> payload_t {
+    return _payload;
+}
+template <u16 HeaderSize, u16 PayloadSize>
+[[nodiscard]] constexpr auto NetworkFrame<HeaderSize, PayloadSize>::toU8Array()
+    const noexcept -> frame_t {
+    frame_t result{};
+    std::copy(_header.cbegin(), _header.cend(), result.begin());
+    std::copy(_payload.cbegin(), _payload.cend(),
+              std::next(result.begin(), HeaderSize));
+    return result;
+}
+template <u16 HeaderSize, u16 PayloadSize>
+[[nodiscard]] constexpr auto
+NetworkFrame<HeaderSize, PayloadSize>::getHeaderRef() noexcept -> header_t& {
+    return _header;
+}
+template <u16 HeaderSize, u16 PayloadSize>
+[[nodiscard]] constexpr auto
+NetworkFrame<HeaderSize, PayloadSize>::getPayloadRef() noexcept -> payload_t& {
+    return _payload;
+}
+
+template <u16 PayloadSize>
+constexpr auto EthernetFrame<PayloadSize>::setHeaderDestinationHwAddr(
+    EthernetFrame::hwaddr_t hwaddr) noexcept -> EthernetFrame& {
+    std::copy(hwaddr.begin(), hwaddr.end(), this->getHeaderRef().begin());
+    return *this;
+}
+template <u16 PayloadSize>
+constexpr auto EthernetFrame<PayloadSize>::setHeaderSourceHwAddr(
+    EthernetFrame::hwaddr_t hwaddr) noexcept -> EthernetFrame& {
+    std::copy(hwaddr.begin(), hwaddr.end(),
+              std::next(this->getHeaderRef().begin(), source_hwaddr_offset));
+    return *this;
+}
+template <u16 PayloadSize>
+constexpr auto EthernetFrame<PayloadSize>::setHeaderPayloadType(
+    u16 payload_type) noexcept -> EthernetFrame& {
+    typename EthernetFrame::header_t& header = this->getHeaderRef();
+    u16ToU8Array(header.begin() + payload_type_offset, payload_type);
+    return *this;
+}
+
+constexpr auto ArpFrame::setHeaderHwAddrType(u16 hwaddr_type) noexcept
+    -> ArpFrame& {
+    header_t& header = this->getHeaderRef();
+    u16ToU8Array(std::next(header.begin(), header_hw_type_offset), hwaddr_type);
+    return *this;
+}
+constexpr auto ArpFrame::setHeaderPrAddrType(u16 praddr_type) noexcept
+    -> ArpFrame& {
+    header_t& header = this->getHeaderRef();
+    u16ToU8Array(std::next(header.begin(), header_pr_type_offset), praddr_type);
+    return *this;
+}
+constexpr auto ArpFrame::setHeaderHwAddrSize(u8 hwaddr_size) noexcept
+    -> ArpFrame& {
+    header_t& header = this->getHeaderRef();
+    header.at(header_hw_size_offset) = hwaddr_size;
+    return *this;
+}
+constexpr auto ArpFrame::setHeaderPrAddrSize(u8 praddr_size) noexcept
+    -> ArpFrame& {
+    header_t& header = this->getHeaderRef();
+    header.at(header_pr_size_offset) = praddr_size;
+    return *this;
+}
+constexpr auto ArpFrame::setHeaderOpcode(u16 opcode) noexcept -> ArpFrame& {
+    header_t& header = this->getHeaderRef();
+    u16ToU8Array(std::next(header.begin(), header_opcode_offset), opcode);
+    return *this;
+}
+constexpr auto ArpFrame::setPayloadSourceHwAddr(
+    ArpFrame::hwaddr_t src_hwaddr) noexcept -> ArpFrame& {
+    payload_t& payload = this->getPayloadRef();
+    std::ranges::copy(src_hwaddr,
+                      std::next(payload.begin(), payload_src_hwaddr_offset));
+    return *this;
+}
+constexpr auto ArpFrame::setPayloadSourcePrAddr(
+    ArpFrame::praddr_t src_praddr) noexcept -> ArpFrame& {
+    payload_t& payload = this->getPayloadRef();
+    std::ranges::copy(src_praddr,
+                      std::next(payload.begin(), payload_src_praddr_offset));
+    return *this;
+}
+constexpr auto ArpFrame::setPayloadDestinationHwAddr(
+    ArpFrame::hwaddr_t dst_hwaddr) noexcept -> ArpFrame& {
+    payload_t& payload = this->getPayloadRef();
+    std::ranges::copy(dst_hwaddr,
+                      std::next(payload.begin(), payload_dst_hwaddr_offset));
+    return *this;
+}
+constexpr auto ArpFrame::setPayloadDestinationPrAddr(
+    ArpFrame::praddr_t dst_praddr) noexcept -> ArpFrame& {
+    payload_t& payload = this->getPayloadRef();
+    std::ranges::copy(dst_praddr,
+                      std::next(payload.begin(), payload_dst_praddr_offset));
+    return *this;
+}
 
 // frame_trait<NetworkFrame<header_size, payload_size>, frame_size, header_size,
 //   payload_size>
--- tests/tests_frames.cpp
+++ tests/tests_frames.cpp
@@ -2,11 +2,10 @@
 #include <gtest/gtest.h>
 
 #include <array>
-#include <print>
 
 #include "frames.hpp"
 
-constexpr u16 default_byte = 0xff;
+constexpr u16 default_byte = 0xFF;
 
 namespace {
 template <u64 Size>
@@ -24,7 +23,6 @@ constexpr auto compareArrayElements(Iter begin, Iter end, u8 value) -> bool {
         }
         std::advance(begin, 1);
     }
-    std::println();
     return true;
 }
 }  // namespace
@@ -66,4 +64,153 @@ TEST(FramesTest, NetworkFrameTou8ArrayWorksCorrectly) {
     ASSERT_TRUE(
         compareArrayElements(std::next(res.begin(), ArpFrame::header_size),
                              res.end(), payload_byte));
+}
+
+TEST(FramesTest, EthernetFrameSetDstHwaddrWorksCorrectly) {
+    EthernetFrame<0> ethernet_frame;
+    constexpr u16 hwaddr_size = ethernet_frame.getHwAddrSize();
+
+    ethernet_frame.setHeaderDestinationHwAddr(
+        generateArraySizeN<hwaddr_size>(default_byte));
+    auto header = ethernet_frame.getHeader();
+
+    ASSERT_EQ(header.at(hwaddr_size + 1), 0x00);
+    ASSERT_TRUE(compareArrayElements(
+        header.begin(), std::next(header.begin(), hwaddr_size), 0xFF));
+}
+
+TEST(FramesTest, EthernetFrameSetSrcHwaddrWorksCorrectly) {
+    EthernetFrame<0> ethernet_frame;
+    constexpr u16 hwaddr_size = ethernet_frame.getHwAddrSize();
+
+    ethernet_frame.setHeaderSourceHwAddr(
+        generateArraySizeN<hwaddr_size>(default_byte));
+    auto header = ethernet_frame.getHeader();
+
+    ASSERT_EQ(header.at(hwaddr_size - 1), 0x00);
+    ASSERT_EQ(header.at((hwaddr_size * 2) + 1), 0x00);
+    ASSERT_TRUE(compareArrayElements(std::next(header.begin(), hwaddr_size),
+                                     std::next(header.begin(), hwaddr_size * 2),
+                                     0xFF));
+}
+
+TEST(FramesTest, EthernetFrameSetPayloadTypeWorksCorrectly) {
+    EthernetFrame<0> ethernet_frame;
+    constexpr u16 hwaddr_size = ethernet_frame.getHwAddrSize();
+    constexpr u16 default_payload_type = 0x0806;
+
+    ethernet_frame.setHeaderPayloadType(default_payload_type);
+    auto header = ethernet_frame.getHeader();
+
+    ASSERT_EQ(header.at((hwaddr_size * 2) - 1), 0x00);
+    ASSERT_EQ(header.at(12), 0x08);
+    ASSERT_EQ(header.at(13), 0x06);
+}
+
+TEST(FramesTest, ArpFrameSetHeaderHwTypeWorksCorrectly) {
+    ArpFrame arp_frame;
+    constexpr u8 hwaddr_type_offset = 0;
+    constexpr u16 ether_proto_type = 0x0001;
+    arp_frame.setHeaderHwAddrType(ether_proto_type);
+
+    ASSERT_EQ(arp_frame.getHeader().at(hwaddr_type_offset), 0x00);
+    ASSERT_EQ(arp_frame.getHeader().at(hwaddr_type_offset + 1), 0x01);
+}
+
+TEST(FramesTest, ArpFrameSetHeaderPrTypeWorksCorrectly) {
+    ArpFrame arp_frame;
+    constexpr u8 praddr_type_offset = 2;
+    constexpr u16 ipv4_proto_type = 0x0800;
+    arp_frame.setHeaderPrAddrType(ipv4_proto_type);
+
+    ASSERT_EQ(arp_frame.getHeader().at(praddr_type_offset), 0x08);
+    ASSERT_EQ(arp_frame.getHeader().at(praddr_type_offset + 1), 0x00);
+}
+
+TEST(FramesTest, ArpFrameSetHeaderHwSizeWorksCorrectly) {
+    ArpFrame arp_frame;
+    constexpr u8 hwaddr_size_offset = 4;
+    constexpr u8 hwaddr_size = 6;
+    arp_frame.setHeaderHwAddrSize(hwaddr_size);
+
+    ASSERT_EQ(arp_frame.getHeader().at(hwaddr_size_offset), 6);
+}
+
+TEST(FramesTest, ArpFrameSetHeaderPrSizeWorksCorrectly) {
+    ArpFrame arp_frame;
+    constexpr u8 praddr_size_offset = 5;
+    constexpr u8 praddr_size = 4;
+    arp_frame.setHeaderPrAddrSize(praddr_size);
+
+    ASSERT_EQ(arp_frame.getHeader().at(praddr_size_offset), 4);
+}
+
+TEST(FramesTest, ArpFrameSetHeaderOpcodeWorksCorrectly) {
+    ArpFrame arp_frame;
+    constexpr u8 opcode_offset = 6;
+    constexpr u16 opcode_request = 0x0001;
+    arp_frame.setHeaderOpcode(opcode_request);
+
+    ASSERT_EQ(arp_frame.getHeader().at(opcode_offset + 1), 0x0001);
+}
+
+TEST(FramesTest, ArpFrameSetSrcHwAddrWorksCorrectly) {
+    ArpFrame arp_frame;
+    constexpr u8 max_byte = 0xFF;
+
+    arp_frame.setPayloadSourceHwAddr(
+        generateArraySizeN<arp_frame.getHwAddrSize()>(max_byte));
+    auto payload = arp_frame.getPayload();
+
+    ASSERT_TRUE(compareArrayElements(
+        payload.begin(), std::next(payload.begin(), arp_frame.getHwAddrSize()),
+        max_byte));
+}
+
+TEST(FramesTest, ArpFrameSetSrcPrAddrWorksCorrectly) {
+    ArpFrame arp_frame;
+    constexpr u8 srcpraddr_offset = 6;
+    constexpr u8 max_byte = 0xFF;
+
+    arp_frame.setPayloadSourcePrAddr(
+        generateArraySizeN<arp_frame.getPrAddrSize()>(max_byte));
+    auto payload = arp_frame.getPayload();
+
+    ASSERT_TRUE(compareArrayElements(
+        std::next(payload.begin(), srcpraddr_offset),
+        std::next(payload.begin(),
+                  srcpraddr_offset + arp_frame.getPrAddrSize()),
+        max_byte));
+}
+
+TEST(FramesTest, ArpFrameSetDstHwAddrWorksCorrectly) {
+    ArpFrame arp_frame;
+    constexpr u8 dsthwaddr_offset = 10;
+    constexpr u8 max_byte = 0xFF;
+
+    arp_frame.setPayloadDestinationHwAddr(
+        generateArraySizeN<arp_frame.getHwAddrSize()>(max_byte));
+    auto payload = arp_frame.getPayload();
+
+    ASSERT_TRUE(compareArrayElements(
+        std::next(payload.begin(), dsthwaddr_offset),
+        std::next(payload.begin(),
+                  dsthwaddr_offset + arp_frame.getHwAddrSize()),
+        max_byte));
+}
+
+TEST(FramesTest, ArpFrameSetDstPrAddrWorksCorrectly) {
+    ArpFrame arp_frame;
+    constexpr u8 dstpraddr_offset = 16;
+    constexpr u8 max_byte = 0xFF;
+
+    arp_frame.setPayloadDestinationPrAddr(
+        generateArraySizeN<arp_frame.getPrAddrSize()>(max_byte));
+    auto payload = arp_frame.getPayload();
+
+    ASSERT_TRUE(compareArrayElements(
+        std::next(payload.begin(), dstpraddr_offset),
+        std::next(payload.begin(),
+                  dstpraddr_offset + arp_frame.getPrAddrSize()),
+        max_byte));
 }
